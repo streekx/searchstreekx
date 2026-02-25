13073deded133598e5f92ef8a1778d7daaa3fab1
streekx <anshkumarchan@gmail.com>
2026-02-25 22:39:49 +0530

    Update SearchResult.kt

--- app/src/main/java/com/streekx/search/SearchResult.kt
+++ app/src/main/java/com/streekx/search/SearchResult.kt
@@ -1,8 +1,2 @@
 package com.streekx.search
-
-data class SearchResult(
-    val title: String,
-    val link: String,
-    val description: String
-)
-
+data class SearchResult(val title: String, val link: String, val description: String)
